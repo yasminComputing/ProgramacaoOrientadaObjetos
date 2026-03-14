7c78a426e12102004f45b391bf8e824b06b164bf
Yasmin <yasmindeitos1959@gmail.com>
2026-03-14 16:57:21 -0300

    Subindo exercicios

---
+++ Exercicio_2/pkg/Computador.java
@@ -0,0 +1,16 @@
+package pkg;
+
+public class Computador {
+		public String marca;
+		public String modelo;
+		public String tipo;
+		public double preco;
+		
+		public void dadosComputador2() {
+			marca = "Dell";
+			modelo = "G15";
+			tipo = "Notebook";
+			preco = 3500f;
+		
+		}
+}
---
+++ Exercicio_4/pkg/Livro.java
@@ -0,0 +1,8 @@
+package pkg;
+
+public class Livro {
+		public String titulo;
+		public String autor;
+		public int  anoDePublicacao;
+		public int edicao;
+}
